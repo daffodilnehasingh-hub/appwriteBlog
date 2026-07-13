007d312375c9c93705382ffae531cb2f7d2dee22
jigglypuff <daffodil.nehasingh@gmail.com>
2026-07-13 14:50:58 +0530

    fix: safe access to image array

--- src/Components/post-form/PostForm.jsx
+++ src/Components/post-form/PostForm.jsx
@@ -22,7 +22,7 @@ function PostForm({ post }) {
 
     const submit = async (data) => {
         if (post) {
-            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null
+            const file = data.image?.[0] ? await appwriteService.uploadFile(data.image[0]) : null
             if (file) {
                 appwriteService.deleteFile(post.featuredImage)
             }
